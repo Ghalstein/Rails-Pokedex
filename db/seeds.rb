0a3f09e1ca2b1ab5742c20cca16f71cb4a8355b3
Garrett Halstein <garrett.halstein@emory.edu>
2019-06-28 15:04:07 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pokemon.destroy_all
+
+Pokemon.create(
+  [
+	{name: "Bulbasaur",
+	weight: 15,
+	description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
+	},
+	{name: "Charmander",
+	weight: 19,
+	description: "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely."
+	},
+	{name: "Squirtle",
+	weight: 20,
+	description: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
+	}
+  ]
+)
